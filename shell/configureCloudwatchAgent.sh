d68269b06f8289a173365c49d510a37e3b032046
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2021-12-15 12:17:40 -0500

    Changes

--- shell/configureCloudwatchAgent.sh
+++ shell/configureCloudwatchAgent.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-if sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m onPremise -s -c ssm:AmazonCloudWatch-linux > /dev/null 2&>1; then
+if sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m onPremise -s -c file:cloudWatchAgentConfig.json > /dev/null 2&>1; then
     echo Configured Cloudwatch agent!
 else
     echo Issue with configuration.
