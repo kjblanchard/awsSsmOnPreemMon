2fd6297feb9f93fad239464d97a3122d5d356e8d
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2021-12-15 13:42:42 -0500

    Changes

--- shell/configureCloudwatchAgent.sh
+++ shell/configureCloudwatchAgent.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m onPremise -s -c file:cloudWatchAgentConfig.json > /dev/null 2&>1 
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m onPremise -s -c file:cloudWatchAgentConfig.json 2>&1 > /dev/null
 code=$?
 
 if [ $code -eq 0 ] ; then
